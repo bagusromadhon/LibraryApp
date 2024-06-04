79fb7ebaa01dc1d332d21073e930e74291188492
bagusromadhon <bagusromadhon007@gmail.com>
2024-06-04 08:57:27 +0700

    [Update] Update Tata Letak

--- LibraryApp/src/main/java/org/example/libraryapp/HelloApplication.java
+++ LibraryApp/src/main/java/org/example/libraryapp/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/org/example/libraryapp/View/student-dashboard.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/org/example/libraryapp/View/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 300, 200);
         stage.setTitle("Login");
         stage.setScene(scene);
